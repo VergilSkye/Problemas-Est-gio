57b19d60d994257f39bf841ecd8c00dcb4b87350
Virgilio <virgiliosps@gmail.com>
2019-02-18 21:15:43 -0300

    Classe com a solucao do problema Album da copa da OBI 2018. 100/100 pontuacao

---
+++ Copa.java
@@ -0,0 +1,81 @@
+import java.awt.List;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer; 
+
+
+
+public class Copa {
+	static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        FastReader s=new FastReader(); 
+        int n = s.nextInt(); 
+        int m = s.nextInt();
+        Set<Integer>figurinhas = new HashSet<Integer>();
+        for(int i =0;i<m;i++) {
+        	figurinhas.add(s.nextInt());
+        }
+        System.out.println(n-figurinhas.size());
+    }
+
+}
